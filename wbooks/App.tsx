842f10ab165fc535f5214fe295e66cb3fc4f6e0c
Cristian Romero <cristian.romero@wolox.co>
2021-05-13 13:25:40 -0500

    configuration Reactotron and debugging test

--- wbooks/App.tsx
+++ wbooks/App.tsx
@@ -1,7 +1,13 @@
+if (__DEV__) {
+  import('./src/config/reactotronConfig').then(() => console!.log('Reactotron Configured'));
+}
+import Reactotron from 'reactotron-react-native';
 import React from 'react';
 
 import App from './src/app';
 
 export default function index() {
+  Reactotron.log('hello rendering world');
+
   return <App />;
 }
