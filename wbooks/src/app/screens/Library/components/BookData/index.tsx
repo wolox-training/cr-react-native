3e604a0b735cb60c25729816637bcf8c11790160
Cristian Romero <cristian.romero@wolox.co>
2021-05-15 19:50:28 -0500

    restructuring of library for the implementation of new features

--- wbooks/src/app/screens/Library/components/BookData/index.tsx
+++ wbooks/src/app/screens/Library/components/BookData/index.tsx
@@ -1,21 +1,11 @@
 import React from 'react';
-import { View, Text, TouchableOpacity, Image, ListRenderItem } from 'react-native';
+import { View, Text, TouchableOpacity, Image } from 'react-native';
 import bcInicio from '@assets/General/bc_inicio.png';
+import Book from '@interfaces/Book';
 
 import styles from './styles';
 
-interface Props {
-  id: number;
-  author: string;
-  title: string;
-  genre: string;
-  publisher: string;
-  year: string;
-  imageUrl: string | null;
-}
-
-const BookData: ListRenderItem<Props> = ({ item }) => {
-  const { author, imageUrl, title } = item;
+const BookData = ({ title, author, imageUrl }: Book) => {
   return (
     <TouchableOpacity style={styles.bookCard}>
       <View style={styles.dataContainer}>
