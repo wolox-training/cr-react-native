fc6ebf5d2a6172da391e135291c05078ec728325
Cristian Romero <cristian.romero@wolox.co>
2021-05-16 16:36:50 -0500

    card 7.2, added layout of bookDetail

--- wbooks/src/app/screens/Library/index.tsx
+++ wbooks/src/app/screens/Library/index.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import { View, FlatList, ListRenderItem } from 'react-native';
 import { BOOKS_MOCK } from '@constants/mockBooks';
 import BookData from '@screens/Library/components/BookData';
-import Book from '@interfaces/Book';
+import Book from '@interfaces/book';
 
 import styles from './styles';
 
-const Library: React.FC = () => {
+const Library = () => {
   const renderItem: ListRenderItem<Book> = ({ item }) => <BookData {...item} />;
   return (
     <View style={styles.container}>
---
+++ wbooks/src/interfaces/navigation.ts
@@ -0,0 +1,6 @@
+import Book from '@interfaces/book';
+
+export type LibraryStackParamList = {
+  Library: undefined;
+  BookDetail: Book;
+};
