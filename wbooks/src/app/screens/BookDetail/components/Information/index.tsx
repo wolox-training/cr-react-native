8a3f2309a3a25cec9ede0a908c79e17f318171a2
Cristian Romero <cristian.romero@wolox.co>
2021-05-18 13:55:40 -0500

    corrections card 7.2

---
+++ wbooks/src/app/screens/BookDetail/components/Information/index.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { View, Text, TouchableOpacity, Image } from 'react-native';
+import bcInicio from '@assets/General/bc_inicio.png';
+
+import styles from './styles';
+
+interface Props {
+  author: string;
+  title: string;
+  genre: string;
+  publisher: string;
+  year: string;
+  imageUrl: string | null;
+}
+
+const Information = ({ title, author, imageUrl, year, genre }: Props) => {
+  return (
+    <View style={styles.bookCard}>
+      <View style={styles.dataContainer}>
+        <Image source={imageUrl ? { uri: imageUrl } : bcInicio} style={styles.detailImageBook} />
+        <View style={styles.textContainer}>
+          <Text style={styles.title}>{title}</Text>
+          <Text style={styles.author}>{author}</Text>
+          <Text style={styles.author}>{year}</Text>
+          <Text style={styles.author}>{genre}</Text>
+        </View>
+      </View>
+      <TouchableOpacity style={styles.btnAdd}>
+        <Text style={styles.btnAddText}>ADD TO WISH LIST</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.btnRent}>
+        <Text style={styles.btnRentText}>RENT</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default Information;
--- wbooks/src/app/screens/BookDetail/index.tsx
+++ wbooks/src/app/screens/BookDetail/index.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { ScrollView } from 'react-native';
+import { StackNavigationProp } from '@react-navigation/stack';
+import { LibraryStackParamList } from '@interfaces/navigation';
+import { RouteProp } from '@react-navigation/native';
+import Information from '@screens/BookDetail/components/Information';
 
 import styles from './styles';
 
-const BookDetail = () => {
+type ProfileScreenNavigationProp = StackNavigationProp<LibraryStackParamList, 'BookDetail'>;
+type ProfileScreenRouteProp = RouteProp<LibraryStackParamList, 'BookDetail'>;
+
+type Props = {
+  route: ProfileScreenRouteProp;
+  navigation: ProfileScreenNavigationProp;
+};
+
+const BookDetail = ({ route }: Props) => {
   return (
-    <View style={styles.container}>
-      <Text>Book Detail</Text>
-    </View>
+    <ScrollView style={styles.container}>
+      <Information {...route.params} />
+    </ScrollView>
   );
 };
 
