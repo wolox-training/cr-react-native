1ada8d4ea1164b5a58bf2463a30f70b89fa7332f
Cristian Romero <cristian.romero@wolox.co>
2021-05-19 12:04:38 -0500

    configuration reactotron-redux

--- wbooks/src/config/reactotronConfig.ts
+++ wbooks/src/config/reactotronConfig.ts
@@ -1,9 +1,13 @@
 import Reactotron from 'reactotron-react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { reactotronRedux } from 'reactotron-redux';
 
-Reactotron.setAsyncStorageHandler(AsyncStorage)
+const reactotron = Reactotron.setAsyncStorageHandler(AsyncStorage)
   .configure({
     name: 'WBooks'
   })
+  .use(reactotronRedux())
   .useReactNative()
   .connect();
+
+export default reactotron;
