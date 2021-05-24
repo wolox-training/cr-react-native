98986921c7e24d8a15ad882b3ba1daab0c8f62ed
Cristian Romero <cristian.romero@wolox.co>
2021-05-24 16:01:32 -0500

    Fix store and reducer

--- wbooks/src/redux/book/reducer.ts
+++ wbooks/src/redux/book/reducer.ts
@@ -1,11 +1,11 @@
-const initialState = {
-  books: []
-};
+import Book from '@interfaces/book';
+
+const initialState: Book[] = [];
 
 const booksReducer = (state = initialState, action: any) => {
   switch (action.type) {
     case 'GET_BOOKS':
-      return { ...state, books: action.payload };
+      return [...state, ...action.payload];
     default:
       return state;
   }
--- wbooks/src/redux/store.ts
+++ wbooks/src/redux/store.ts
@@ -1,5 +1,9 @@
-import { createStore } from 'redux';
+import { createStore, combineReducers } from 'redux';
 import Reactotron from '@config/reactotronConfig';
 import bookReducer from '@redux/book/reducer';
 
-export default createStore(bookReducer, Reactotron.createEnhancer());
+const rootReducer = combineReducers({
+  books: bookReducer
+});
+
+export default createStore(rootReducer, Reactotron.createEnhancer());
