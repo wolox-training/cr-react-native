65449c47927f3ec20569823b0ed8c18d5aa4de66
Cristian Romero <cristian.romero@wolox.co>
2021-05-26 13:53:59 -0500

    Fix App.test from thunk

--- wbooks/App.test.tsx
+++ wbooks/App.test.tsx
@@ -5,8 +5,14 @@
 import 'react-native';
 import React from 'react';
 import renderer from 'react-test-renderer';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
 
 import App from './src/app';
+
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
 
 const reactotron = {
   configure: () => reactotron,
@@ -20,6 +26,14 @@ const reactotron = {
 jest.mock('reactotron-react-native', () => reactotron);
 // Note: test renderer must be required after react-native.
 
-it('renders correctly', () => {
-  renderer.create(<App />);
+describe('async actions', () => {
+  const store = mockStore();
+
+  it('renders correctly', () => {
+    renderer.create(
+      <Provider store={store}>
+        <App />
+      </Provider>
+    );
+  });
 });
