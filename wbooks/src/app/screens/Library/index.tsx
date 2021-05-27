13a511616dd2de9e01e38fa1765a6cff37ed61eb
Cristian Romero <cristian.romero@wolox.co>
2021-05-26 20:35:41 -0500

    Fix useSelector in Library

--- wbooks/src/app/screens/Library/index.tsx
+++ wbooks/src/app/screens/Library/index.tsx
@@ -18,7 +18,7 @@ const Library = () => {
     dispatch(actionCreators.getBooks());
   }, [dispatch]);
 
-  const books = useSelector((state: RootState) => state.book.books);
+  const { books } = useSelector((state: RootState) => state.book);
 
   const renderItem: ListRenderItem<Book> = ({ item }) => <BookData {...item} />;
 
